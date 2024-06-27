6fb81d7947534d2c973723b82db38c31bb1f1ec6
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2024-06-27 15:12:09 -0400

    Completed Concatenate Two Strings Exercise

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        String result = str1 + " " + str2;
+        System.out.println(result);
+    }
+}
